68d32dd997b89f50659f9feb5ddd35b46340ddc8
Milad Fatenejad <icksa1@gmail.com>
2015-08-16 13:48:05 -0500

    Added configuration options for using mysql

--- src/main/java/net/epv/epvserver/EpvServerConfiguration.java
+++ src/main/java/net/epv/epvserver/EpvServerConfiguration.java
@@ -3,7 +3,9 @@ package net.epv.epvserver;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -17,11 +19,16 @@ public class EpvServerConfiguration extends Configuration {
     @NotNull
     private final String userName;
 
+    @Valid
+    @NotNull
+    private final DataSourceFactory database;
+
     @JsonCreator
     public EpvServerConfiguration(@JsonProperty("userName") String userName,
                                   @JsonProperty("sha256Password") String sha256Password) {
         this.userName = userName;
         this.sha256Password = sha256Password;
+        this.database = new DataSourceFactory();
     }
 
     @JsonProperty("userName")
@@ -32,5 +39,10 @@ public class EpvServerConfiguration extends Configuration {
     @JsonProperty("sha256Password")
     public String getSha256Password() {
         return sha256Password;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDatabase() {
+        return database;
     }
 }
