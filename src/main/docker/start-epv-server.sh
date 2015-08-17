16b28f3c01c305e499b2eb804c77827d653a3f9c
Milad Fatenejad <icksa1@gmail.com>
2015-08-16 20:00:29 -0500

    Added environment variables for setting credentials

--- src/main/docker/start-epv-server.sh
+++ src/main/docker/start-epv-server.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 set -e
 
-sed -i "s/%MYSQL_PORT_3306_TCP_PORT%/${MYSQL_PORT_3306_TCP_PORT}/" /opt/epv-server/epv-server.yml
-sed -i "s/%MYSQL_USER%/${MYSQL_ENV_MYSQL_USER}/" /opt/epv-server/epv-server.yml
-sed -i "s/%MYSQL_PASSWORD%/${MYSQL_ENV_MYSQL_PASSWORD}/" /opt/epv-server/epv-server.yml
+sed -i "s/%MYSQL_PORT_3306_TCP_PORT%/${MYSQL_PORT_3306_TCP_PORT?}/" /opt/epv-server/epv-server.yml
+sed -i "s/%MYSQL_USER%/${MYSQL_ENV_MYSQL_USER?}/" /opt/epv-server/epv-server.yml
+sed -i "s/%MYSQL_PASSWORD%/${MYSQL_ENV_MYSQL_PASSWORD?}/" /opt/epv-server/epv-server.yml
+sed -i "s/%EPV_SERVER_SHA256_PASSWORD%/${EPV_SERVER_SHA256_PASSWORD?}/" /opt/epv-server/epv-server.yml
+sed -i "s/%EPV_SERVER_USER%/${EPV_SERVER_USER?}/" /opt/epv-server/epv-server.yml
 
 # Wait for MySQL to Start:
-MYSQL_LOOPS="120"
+num_attempts="120"
 i=0
 while ! nc mysql ${MYSQL_PORT_3306_TCP_PORT} >/dev/null 2>&1 < /dev/null; do
     i=$(expr $i + 1)
-    if [ $i -ge "${MYSQL_LOOPS}" ]; then
-        echo "$(date) - mysql:${MYSQL_PORT_3306_TCP_PORT} still not reachable, giving up"
+    if [ $i -ge "${num_attempts}" ]; then
+        echo "$(date) - mysql:${MYSQL_PORT_3306_TCP_PORT?} still not reachable, giving up"
         exit 1
     fi
-    echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT_3306_TCP_PORT}..."
+    echo "$(date) - waiting for mysql:${MYSQL_PORT_3306_TCP_PORT?}..."
     sleep 1
 done
 
 # Start EPV Server:
+DEBUG=${DEBUG:-false}
 if [ "${DEBUG}" == "true" ]; then
-    DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n"
+    debug_opts="-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n"
 fi
 
-java ${DEBUG_OPTS} -jar /opt/epv-server/epvserver.jar server /opt/epv-server/epv-server.yml
+java ${debug_opts} -jar /opt/epv-server/epvserver.jar server /opt/epv-server/epv-server.yml
